4b0183419b1f05b3ad574b3897203ebdef244cd1
Davide Lodde <johndoe@example.com>
2024-05-02 19:55:51 +0200

    navbar

--- src/Home/HeaderHome.jsx
+++ src/Home/HeaderHome.jsx
@@ -1,19 +1,33 @@
-import { Logo } from "./Logo"
-import './css/headerHome.css'
+import { Logo } from "./Logo";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Button from 'react-bootstrap/Button';
+import "./css/headerHome.css";
+
 
 export function HeaderHome() {
-    const headerStyle = {
-        height: "70px",
-        width: "100vw",
-        backgroundColor: "#212529",
-        display: "flex",
-        flexOrientation: "row"
-
-    }
-    return(
-        <div style={headerStyle}>
-            <Logo />
-        </div>
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home"><Logo /></Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#link">Dati Anagrafici</Nav.Link>
+              <Nav.Link href="#link">I Miei Studi</Nav.Link>
+              <Nav.Link href="#link">I Miei Lavori</Nav.Link>
+              <Nav.Link href="#link">Contatti</Nav.Link>
+              
+            </Nav>
+            
+          </Navbar.Collapse>
+          <Button variant="primary">Primary</Button>
+        </Container>
         
-    )
+      </Navbar>
+      
+    </>
+  );
 }
--- src/Home/MainHome.jsx
+++ src/Home/MainHome.jsx
@@ -8,11 +8,12 @@ export function MainHome() {
       <div className="hero">
         <img className="imageHero" src="public/heroHome.png" alt="hero" />
       </div>
+      <h2>Le Mie Skills</h2>
       <div className="cards">
         
-        <Cards classe="cardSx" imageSrc="public/vite.svg" imageAlt="vite" title="Web" description1="In questa sezione potrai scoprire le mie competenze come" description2="Junior FullStack Web Developer" href="#" />
-        <Cards classe="cardCenter" imageSrc="public/vite.svg" imageAlt="vite" title="Android" description1="In questa sezione potrai scoprire le mie competenze come" description2="Senior Android Developer"  href="#" />
-        <Cards classe="cardDx" imageSrc="public/vite.svg" imageAlt="vite" title="iOS" description1="In questa sezione potrai scoprire le mie competenze come" description2="Junior iOS Developer"  href="#" />
+        <Cards classe="cardSx" imageSrc="public/cardWeb.jpeg" imageAlt="vite" title="Web" description1="In questa sezione potrai scoprire le mie competenze come" description2="Junior FullStack Web Developer" href="#" />
+        <Cards classe="cardCenter" imageSrc="public/cardAndroid.jpeg" imageAlt="vite" title="Android" description1="In questa sezione potrai scoprire le mie competenze come" description2="Senior Android Developer"  href="#" />
+        <Cards classe="cardDx" imageSrc="public/cardIOS.jpeg" imageAlt="vite" title="iOS" description1="In questa sezione potrai scoprire le mie competenze come" description2="Junior iOS Developer"  href="#" />
         
       </div>
     </div>
