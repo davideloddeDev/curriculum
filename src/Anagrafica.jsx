65783e32c657658e7c35b4c5dea5f2561ed28908
Davide Lodde <johndoe@example.com>
2024-07-12 11:24:10 +0200

    implementazioni pagina anagrafica

--- src/Anagrafica.jsx
+++ src/Anagrafica.jsx
@@ -2,8 +2,26 @@ import './Anagrafica/css/anagrafica.css'
 
 export function Anagrafica() {
     return(
-        <div>
-            <h1>Anagrafica</h1>
+        <div className="anagrafica">
+            <section className="anagraficaHero">
+                <img src="heroAnagrafici.jpeg"
+                     alt="hero"/>
+            </section>
+            <section className="anagraficaDescription">
+                <div className="containerSx">
+                    <img src="DavideLodde.png"
+                         alt="Davide Lodde"/>
+                </div>
+                <div className="containerDx">
+                    <h2>Mi Presento</h2>
+                    <p>
+                        Mi chiamo Davide Lodde, sono nato a Cagliari il 08/10/1993,<br/>
+                        ho 30 anni e sono uno svilluppatore software appassionato di tecnologia.<br/><br/>
+
+                        Sono di nazionalità italiana e vivo a Giugliano in Campania, in provincia di Napoli.
+                    </p>
+                </div>
+            </section>
         </div>
     )
 }
